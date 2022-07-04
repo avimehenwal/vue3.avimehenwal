de1f7f74dcce05d32c7e51f7490686ca2f92e83d
avimehenwal <avi.mehanwal@gmail.com>
2022-07-04 14:08:34 +0200

    feat(vue): :rocket: permission based menu visibility

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Role based menu example</p>
+    <ul v-for="menu in menuItems" :key="menu.name">
+      <li v-if="menu.isMenuAllowed()">{{ menu.icon }} -> {{ menu.name }}</li>
+    </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isAuthenticated = true;
+const isAdmin = true;
+
+const menuItems = ref([
+  {
+    name: "first_menu",
+    icon: "icon1",
+    isMenuAllowed: () => (isAuthenticated ? true : false),
+  },
+  {
+    name: "second_menu",
+    icon: "icon2",
+    isMenuAllowed: () => (isAuthenticated && isAdmin ? true : false),
+  },
+]);
+</script>
 
 <style>
 @media (min-width: 1024px) {
